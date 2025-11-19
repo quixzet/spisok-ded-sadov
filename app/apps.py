c83d7b85fd1d01a1a05ae53d1f9a4ecd51b63764
Студент группы IS <is@sielom.local>
2025-11-19 14:15:57 +0500

    f

--- app/apps.py
+++ app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Standoff2Config(AppConfig):
+class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'standoff2'
+    name = 'app'
--- new/urls.py
+++ new/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for main project.
+URL configuration for new project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from standoff2 import views  # добавьте этот импорт
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
